5ff9a1eb6ad71f9c82f5f6ccefbda3eb93ca29ca
akash-d <akashdeep001@gmail.com>
2017-11-03 11:13:47 +0530

    time changed

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var app = angular.module("app", ["highcharts-ng"]);
 
-app.controller("BarCtrl", ["$scope", "$http", "$interval", function ($scope, $http, $interval) {
+app.controller("BarCtrl", ["$scope", "$http", "$interval", function($scope, $http, $interval) {
 
     function callAPI() {
         $http({
             url: 'https://api.coinmarketcap.com/v1/ticker/?limit=10',
             method: 'GET'
         }).then(
-            function (resp) {
+            function(resp) {
                 console.log(resp.data);
                 generateData(resp.data);
             },
-            function () {
+            function() {
                 alert("no data")
             }
         )
@@ -22,24 +22,23 @@ app.controller("BarCtrl", ["$scope", "$http", "$interval", function ($scope, $ht
         chart: {
             type: 'column',
             events: {
-                load: function () {
+                load: function() {
                     // set up the updating of the chart each second
                     var series = this.series[0];
-                    setInterval(function () {
+                    setInterval(function() {
                         $http({
                             url: 'https://api.coinmarketcap.com/v1/ticker/?limit=10',
                             method: 'GET'
-                        }).then(function (resp) {
-                                console.log(resp.data);
-                                angular.forEach(resp.data, function (value, key) {
-                                    var temp = {};
-                                    temp.name = value.name;
-                                    temp.y = parseFloat(value.price_usd);
-                                    series.addPoint(temp,true);
-                                });
-                            }
-                        )
-                    }, 5000);
+                        }).then(function(resp) {
+                            console.log(resp.data);
+                            angular.forEach(resp.data, function(value, key) {
+                                var temp = {};
+                                temp.name = value.name;
+                                temp.y = parseFloat(value.price_usd);
+                                series.addPoint(temp, true);
+                            });
+                        })
+                    }, 5 * 60 * 1000);
                 }
             }
         },
@@ -50,7 +49,7 @@ app.controller("BarCtrl", ["$scope", "$http", "$interval", function ($scope, $ht
             enabled: false
         },
         title: {
-            text: 'Exchange rate of all crypto currencies',
+            text: 'Exchange rate of all crypto currencies in USD',
             align: 'left',
             x: 70
         },
@@ -67,21 +66,20 @@ app.controller("BarCtrl", ["$scope", "$http", "$interval", function ($scope, $ht
         series: [{
             name: 'data',
             colorByPoint: true,
-            data: (function () {
+            data: (function() {
                 var t = [];
                 $http({
                     url: 'https://api.coinmarketcap.com/v1/ticker/?limit=10',
                     method: 'GET'
-                }).then(function (resp) {
-                        console.log(resp.data);
-                        angular.forEach(resp.data, function (value, key) {
-                            var temp = {};
-                            temp.name = value.name;
-                            temp.y = parseFloat(value.price_usd);
-                            t.push(temp);
-                        });
-                    }
-                )
+                }).then(function(resp) {
+                    console.log(resp.data);
+                    angular.forEach(resp.data, function(value, key) {
+                        var temp = {};
+                        temp.name = value.name;
+                        temp.y = parseFloat(value.price_usd);
+                        t.push(temp);
+                    });
+                })
                 return t;
             }())
         }]
@@ -91,7 +89,7 @@ app.controller("BarCtrl", ["$scope", "$http", "$interval", function ($scope, $ht
 
         if (val.length === t.length) {
             $scope.graphData = t;
-            console.log("called",$scope.graphData)
+            console.log("called", $scope.graphData)
 
         }
     }
